04b959ff91b3f57dac9430d7bf7ba19ddb88cf06
William Cohen <wcohen@redhat.com>
2020-10-02 13:53:54 -0400

    Switch to using eu-readelf to get line and column information
    eu-readelf understands split dwarf which dwarfdump doesn't.  Also
    eu-readelf does a better job generating the filenames.  There were
    cases where dwarfdump would return filenames like the following:
    
    <DW_FORM_GNU_strp_alt-no-tied-file>/pfmlib_intel_netburst.c
    
    Switched to using eu-readelf to get more complete
    filename:line:column info.

--- bin/line_column_info.py
+++ bin/line_column_info.py
@@ -13,16 +13,14 @@ def munge_line(line):
     # output
     # file:linue:column
     global filename
+    #find if there is a file name on line
+    if re.search(r'\(mtime: (\d+), length: (\d+)\)$', line):
+        filename = re.sub(r'^\s*([^\s]+)[^.]*', r'\1', line)
     #find if address on line
-    if re.match("^0x", line):
-        lineno_column = re.search(r'(\d+),\s*(\d+)', line).group()
-        lineno = re.sub(r'(\d+),\s*(\d+)', r'\1', lineno_column)
-        column = re.sub(r'(\d+),\s*(\d+)', r'\2', lineno_column)
-        #find if there is a file name on line
-        if re.search(r' uri: ', line):
-            # get file name and strip off quotes at beginning and end
-            filename = re.search(r'\"[^\"]+\"', line).group()
-            filename = re.search(r'[^\"]+', filename).group()
+    if re.match("^\s*(\d+):(\d+)", line):
+        lineno_column = re.search(r'^\s*(\d+):(\d+)', line).group()
+        lineno = re.sub(r'^\s*(\d+):(\d+)', r'\1', lineno_column)
+        column = re.sub(r'^\s*(\d+):(\d+)', r'\2', lineno_column)
         #generate munged line
         print(f'{filename}:{lineno:0>6}:{column:0>3}')
 
@@ -46,7 +44,7 @@ def find_debuginfo(fileointerest):
 
 def process_file(fileointerest):
     fileointerest = find_debuginfo(fileointerest)
-    process = subprocess.Popen(['dwarfdump', '--print-lines', fileointerest], 
+    process = subprocess.Popen(['eu-readelf', '--debug-dump=decodedline', fileointerest],
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
     for output in process.stdout:
